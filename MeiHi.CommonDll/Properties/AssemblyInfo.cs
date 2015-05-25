2a24dd3a25782556ee818afec81cfe90d892f69e
qcli <qcli@Ctrip.com>
2015-05-25 11:10:01 +0800

    fix

--- MeiHi.CommonDll/Properties/AssemblyInfo.cs
+++ MeiHi.CommonDll/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MeiHi.CommonDll")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("MeiHi.CommonDll")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("78918f78-e24f-4b1d-9b24-da40fb5d0252")]
+[assembly: Guid("ce546f90-f432-4e0b-a917-56eabdf66906")]
 
 // Version information for an assembly consists of the following four values:
 //
